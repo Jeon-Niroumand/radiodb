838a369bf85dd1b3a1e08dd628cbb3baa5bdeb2b
Jeon Niroumand <Jeon-Niroumand@scusd.edu>
2026-07-22 15:20:36 -0700

    session.save

--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,9 +1,7 @@
 import express from "express";
-import passport from "./passport.js";
-
+import passport from "./passport.js"
 import {
   googleLogin,
-  loginSuccess,
   logout,
   currentUser,
 } from "./authController.js";
@@ -12,17 +10,50 @@ const router = express.Router();
 
 console.log("AUTH ROUTES LOADED");
 
-router.get("/google", googleLogin);
+router.get("/google", (req, res, next) => {
+  console.log("GOOGLE LOGIN ROUTE HIT");
+  googleLogin(req, res, next);
+});
 
 router.get(
   "/google/callback",
   passport.authenticate("google", {
     failureRedirect: `${process.env.CLIENT_URL}/login?error=auth`,
   }),
-  loginSuccess
+  (req, res, next) => {
+
+    console.log("AUTH SUCCESS USER:", req.user);
+
+    req.session.save(err => {
+      if (err) {
+        console.error("SESSION SAVE ERROR:", err);
+        return next(err);
+      }
+
+      console.log("SESSION ID BEFORE REDIRECT:", req.sessionID);
+
+      console.log(
+        "SET COOKIE HEADER:",
+        res.getHeader("Set-Cookie")
+      );
+
+      res.redirect(process.env.CLIENT_URL);
+    });
+
+  }
 );
 
-router.get("/me", currentUser);
+router.get("/me", (req, res) => {
+  console.log("AUTH /me CHECK");
+   console.log("REQ HEADERS COOKIE:", req.headers.cookie);
+  console.log("SESSION ID:", req.sessionID);
+  console.log("SESSION:", req.session);
+  console.log("USER:", req.user);
+
+  res.json({
+    user: req.user || null,
+  });
+});
 
 router.post("/logout", logout);
 
